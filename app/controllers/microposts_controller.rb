8adcdffd0aacc0877d56b7b0d80731cf4856c40b
github shunyonai1996 <y_hhns_1028@icloud.com>
2022-06-19 14:30:57 +0900

    post destroy complete

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: %i[create destroy]
+  before_action :correct_user, only: :destroy
 
   def index
     @users = User.where(activated: true)
@@ -21,13 +22,17 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = "投稿を削除しました"
+    redirect_to request.referrer || root_url
   end
 
   def search
-    if params[:job].present?
-      @microposts = Micropost.where("job LIKE ?", "%#{params[:job]}%").paginate(page: params[:page])
+    if params[:id].present?
+      @microposts = Micropost.where("occupation_id LIKE ?", "%#{params[:id]}%").paginate(page: params[:page])
+      # @microposts = Micropost.where("job LIKE ?", "%#{params[:occupation_id]}%").paginate(page: params[:page])
     else
-      flash[:danger] = "検索結果にマッチする投稿がありません"
+      flash[:danger] = "職種を選択してください"
       redirect_to root_path
     end
   end
@@ -35,5 +40,10 @@ class MicropostsController < ApplicationController
   private
     def micropost_params
       params.require(:micropost).permit(:occupation_id, :job, :content, :content_2, :content_3, :content_4, :content_5, :content_6, :content_7, :content_8, :content_9, :content_10, :time, :time_2, :time_3, :time_4, :time_5, :time_6, :time_7, :time_8, :time_9, :time_10)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 end
