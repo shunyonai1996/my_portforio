3b34ce56fa7ef9d025f9e0109f4070c677bbfbac
github shunyonai1996 <y_hhns_1028@icloud.com>
2022-08-30 00:30:09 +0900

    #11 ユーザー一覧表示エラー修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      flash[:success] = 'ログイン成功'
+      flash[:success] = 'ログインしました'
       redirect_back_or user
     else
       flash.now[:danger] = 'ログイン失敗｜ メールアドレスまたはパスワードに誤りがあります'
@@ -17,6 +17,7 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out if logged_in?
+    flash[:success] = 'ログアウトしました'
     redirect_to root_url
   end
 end
